d37bdeecca37d20c98e6fbb69b72332c2e9c987f
vishnuvardhangude <vishnuvardhangude@gmail.com>
2026-02-10 15:57:12 +0800

    tesla clone commit

---
+++ src/App.jsx
@@ -0,0 +1,22 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import Header from './components/Header';
+import Home from './components/Home';
+import Section from './components/Section';
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <div className='App'>
+        {/* <Header /> */}
+        {/* <Home /> */}
+        <Section />
+      </div>
+    </>
+  )
+}
+
+export default App
---
+++ src/components/Section.jsx
@@ -0,0 +1,73 @@
+import React from 'react'
+import styled from 'styled-components';
+
+function Section() {
+  return (
+    <Wrap>
+      <ItemText>
+        <h1>Model S</h1>
+        <p>Order Online for Touchless Delivery </p>
+      </ItemText>
+      <Buttons>
+      <ButtonGroup>
+        <LeftButton>Custom Order</LeftButton>
+        <RightButton>Existing Inventory</RightButton>
+      </ButtonGroup>
+      <DownArrow src="/images/down-arrow.svg" />
+      </Buttons>
+
+
+    </Wrap>
+  )
+}
+
+export default Section
+
+const Wrap = styled.div`
+  height: 100vh;
+  width: 100vw;
+  background-size: cover;
+  background-position: center;
+  background-repeat: no-repeat;
+  background-image: url('/images/model-s.jpg');
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between; //vertical
+  align-items: center; //horizantal
+  
+`
+
+const ItemText = styled.div`
+  padding-top: 15vh;
+  text-align: center;
+`
+
+const ButtonGroup = styled.div`
+  display: flex;
+  margin-bottom: 30px;
+`
+
+const LeftButton= styled.div`
+  background-color: rgba(94, 68, 94, 0.8);
+  height: 40px;
+  width: 256px;
+  color: white;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  border-radius: 100px;
+  opacity: 1.0;
+  text-transform: uppercase;
+  font-size: 12px;
+  cursor: pointer;
+`
+
+const RightButton=styled(LeftButton)`
+`
+
+const DownArrow=styled.img`
+  margin-top: 20px;
+  height: 40px;
+`
+
+const Buttons=styled.div``
